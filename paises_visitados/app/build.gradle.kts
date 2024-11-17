b383bec127e982def15b1c0bfbd2b9d51e906c5c
bolo-stacks <bolo-stacks@outlook.com>
2024-11-17 12:21:20 -0300

    v71

--- paises_visitados/app/build.gradle.kts
+++ paises_visitados/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.paises_visitados"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.paises_visitados"
