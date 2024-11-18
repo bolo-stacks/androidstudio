8eaa88c7f1642988a518e3b3bd420af9aa0d3011
bolo-stacks <bolo-stacks@outlook.com>
2024-11-17 21:08:12 -0300

    v89

---
+++ spotifeio/app/src/main/java/com/example/spotifeio/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.spotifeio
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.spotifeio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var mediaPlayer : MediaPlayer
+    private var playing = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgBaitaca.setOnClickListener {
+            if (playing) mediaPlayer.stop()
+            mediaPlayer = MediaPlayer.create(applicationContext, R.raw.baitaca_musica)
+            mediaPlayer.start()
+            playing = true
+        }
+
+        binding.imgBaitaca.setOnClickListener {
+            if (playing) mediaPlayer.stop()
+            mediaPlayer = MediaPlayer.create(applicationContext, R.raw.baitaca_musica)
+            mediaPlayer.start()
+            playing = true
+        }
+
+        binding.imgBaitaca.setOnClickListener {
+            if (playing) mediaPlayer.stop()
+            mediaPlayer = MediaPlayer.create(applicationContext, R.raw.baitaca_musica)
+            mediaPlayer.start()
+            playing = true
+        }
+    }
+}
